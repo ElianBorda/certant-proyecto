94434ea95fde2f45e15b2075fb0193649f5fbd80
Elian Borda <eliancamiloalejandro@gmail.com>
2024-03-28 11:14:04 -0300

    init proyecto

---
+++ src/main/java/com/certan/boutiquedelautomotor/BoutiquedelautomotorApplication.java
@@ -0,0 +1,13 @@
+package com.certan.boutiquedelautomotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiquedelautomotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiquedelautomotorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certan/boutiquedelautomotor/BoutiquedelautomotorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certan.boutiquedelautomotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiquedelautomotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
